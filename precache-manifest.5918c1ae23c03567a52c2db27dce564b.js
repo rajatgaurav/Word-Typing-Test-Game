50afb4ca1887a4f0016d018d7a7ff45038ac2310
RAJAT GAURAV <53166907+rajatgaurav@users.noreply.github.com>
2022-07-13 22:49:23 +0530

    Updates

---
+++ precache-manifest.5918c1ae23c03567a52c2db27dce564b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d81cd9bb850805c009",
+    "url": "/Word-Typing-Test-Game/static/css/main.2b34446f.chunk.css"
+  },
+  {
+    "revision": "99d81cd9bb850805c009",
+    "url": "/Word-Typing-Test-Game/static/js/main.99d81cd9.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/Word-Typing-Test-Game/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "9d703b4fa4613e4fb324",
+    "url": "/Word-Typing-Test-Game/static/js/2.9d703b4f.chunk.js"
+  },
+  {
+    "revision": "050db5321af7938eb741",
+    "url": "/Word-Typing-Test-Game/static/js/runtime~main.050db532.js"
+  },
+  {
+    "revision": "42965189479ee92028ff084b59b6ad08",
+    "url": "/Word-Typing-Test-Game/index.html"
+  }
+];
